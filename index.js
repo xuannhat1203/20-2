e5da7c42920f0b0428056dd4a57c14f0c151321a
xuannhat1203 <141288976+xuannhat1203@users.noreply.github.com>
2024-02-20 08:03:36 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let item = document.createElement("item");
+let infor = document.getElementsByClassName("infor");
+item.innerHTML = `{
+    <span class="material-symbols-outlined">feed</span>
+    <h3>TIN TỨC & ƯU ĐÃI</h3>
+    <span class="material-symbols-outlined">local_activity</span>
+    <h3>VÉ CỦA TÔI</h3>
+    <span class="material-symbols-outlined">person</span>
+    <button id="sign in>ĐĂNG NHẬP</button>
+    <button id="create>ĐĂNG KÍ</button>
+}`
+item.appendChild(infor);
